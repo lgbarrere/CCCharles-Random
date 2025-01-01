6e815339a9a0533621a0b598ed839d903f658bf0
lgbarrere <louis.gabriel.barrere@gmail.com>
2025-01-01 15:55:05 +0100

    Try hooks for Sasha's Mission + clean sources
    > All C++ source files are now removed because that cannot be used easily in mods (use dllmain.cpp instead)
    > Test hooks to save the number of gathered pages (work in progress)

--- ModSrc/RandomizerMod/dllmain.cpp
+++ ModSrc/RandomizerMod/dllmain.cpp
@@ -3,6 +3,9 @@
 #include <Unreal/UObjectGlobals.hpp>
 #include <Unreal/UObject.hpp>
 #include <Unreal/Hooks.hpp>
+#include <Unreal/UFunction.hpp>
+#include <Unreal/FProperty.hpp>
+
 /*
 #include <Runtime/Core/Public/Misc/Paths.hpp>
 #include <Runtime/Core/Public/HAL/PlatformFilemanager.hpp>
@@ -52,11 +55,31 @@ public:
     //     return result;
     // }
 
+    static auto CallbackFunctionHook([[maybe_unused]] Unreal::UObject* Context, Unreal::FFrame& Stack, [[maybe_unused]] void* RESULT_DECL) -> void
+    {
+        // In the callback, or anywhere else on or after on_unnreal_init.
+        static auto FuncNameToHook = FName(STR("FuncNameToHook"), FNAME_Add);
+
+        // Check the function name is correct in the callback
+        if (Stack.Node()->GetNamePrivate() != FuncNameToHook)
+        {
+            // Unexpected function, so exit early
+            return;
+        }
+        FString* tmpParam = Stack.Node()->GetPropertyByName(STR("SomeParamHook"))->ContainerPtrToValuePtr<FString>(Stack.Locals());
+        int32* tmpParam2 = Stack.Node()->GetPropertyByName(STR("AnotherParamHook"))->ContainerPtrToValuePtr<int32>(Stack.Locals());
+        
+        Output::send<LogLevel::Verbose>(STR("{}"), tmpParam->GetCharArray());
+        Output::send<LogLevel::Verbose>(STR("{}"), *tmpParam2);
+
+        Output::send<LogLevel::Verbose>(STR("FuncNameToHook has been called with success\n"));
+    }
+
     void StartArchipelagoConnexion(const char* ipAddress, const char* gameName, const char* playerName, const char* password)
     {
         // IP example from /connect archipelago.gg:38281 : IP = archipelago.gg:38281
-        // AP_Init("", "Choo-Choo charles", "YaranCCC", "");
-        AP_Init(ipAddress, gameName, playerName, password);
+        // AP_Init("", "Choo-Choo charles", "YaranCCC", ""); // Raw values for test
+        AP_Init(ipAddress, gameName, playerName, password); // Using variables filled by the user and Archipelago
     }
 
     auto on_unreal_init() -> void override
@@ -89,6 +112,13 @@ public:
             // StartArchipelagoConnexion("localhost", "Choo-Choo charles", "YaranCCC", "");
             return false;
         });
+
+        /// TEST OF HOOKS
+        if (Unreal::UObject::ProcessLocalScriptFunctionInternal.is_ready() && Unreal::Version::IsAtLeast(4, 22))
+        {
+            Output::send(STR("Enabling custom events\n"));
+            Unreal::Hook::RegisterProcessLocalScriptFunctionPostCallback(CallbackFunctionHook);
+        }
     }
 
     auto on_update() -> void override
