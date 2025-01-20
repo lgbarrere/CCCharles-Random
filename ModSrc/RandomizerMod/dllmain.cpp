ff5dfd9570a9206ba12d4eda6a116ab2d5cf540d
lgbarrere <louis.gabriel.barrere@gmail.com>
2025-01-20 01:14:38 +0100

    All item randomizations prepared
    > All items can be spawned through BP (paint cans, guns and page drawing now covered)
    ! Note all [Test Module] are still to move in appropriate places and the check locations are to link to these implementations
    > Function hooking examples added in dllmain.cpp
    > Command parser added in ModConsole.cpp

--- ModSrc/RandomizerMod/dllmain.cpp
+++ ModSrc/RandomizerMod/dllmain.cpp
@@ -4,6 +4,7 @@
 #include <Unreal/UObject.hpp>
 #include <Unreal/Hooks.hpp>
 #include <Unreal/UFunction.hpp>
+#include <Unreal/UFunctionStructs.hpp>
 #include <Unreal/FProperty.hpp>
 
 /*
@@ -59,6 +60,7 @@ public:
     {
         // In the callback, or anywhere else on or after on_unnreal_init.
         static auto FuncNameToHook = FName(STR("FuncNameToHook"), FNAME_Add);
+        static auto FuncNameToCall = FName(STR("FuncNameToCall"), FNAME_Add);
 
         // Check the function name is correct in the callback
         if (Stack.Node()->GetNamePrivate() != FuncNameToHook)
@@ -66,20 +68,55 @@ public:
             // Unexpected function, so exit early
             return;
         }
+
+        // The function name is correct, proceed
         FString* tmpParam = Stack.Node()->GetPropertyByName(STR("SomeParamHook"))->ContainerPtrToValuePtr<FString>(Stack.Locals());
         int32* tmpParam2 = Stack.Node()->GetPropertyByName(STR("AnotherParamHook"))->ContainerPtrToValuePtr<int32>(Stack.Locals());
-        
+        int32* tmpRes = Stack.Node()->GetPropertyByName(STR("SomeResultHook"))->ContainerPtrToValuePtr<int32>(Stack.Locals());
+
+        int32* res = (int32*)RESULT_DECL;
+        *res = 4; // Not working
+
         Output::send<LogLevel::Verbose>(STR("{}"), tmpParam->GetCharArray());
         Output::send<LogLevel::Verbose>(STR("{}"), *tmpParam2);
 
         Output::send<LogLevel::Verbose>(STR("FuncNameToHook has been called with success\n"));
+
+        // Call a BP function to pass parameters
+        FString itemName = FString(STR("Some_item"));
+        Stack.Object()->ProcessEvent(Stack.Object()->GetFunctionByName(FuncNameToCall), &itemName);
     }
 
     void StartArchipelagoConnexion(const char* ipAddress, const char* gameName, const char* playerName, const char* password)
     {
         // IP example from /connect archipelago.gg:38281 : IP = archipelago.gg:38281
-        // AP_Init("", "Choo-Choo charles", "YaranCCC", ""); // Raw values for test
+        // AP_Init("localhost:38281", "Choo-Choo charles", "YaranCCC", ""); // Raw values for test
         AP_Init(ipAddress, gameName, playerName, password); // Using variables filled by the user and Archipelago
+    }
+
+    static bool CallbackConsole(UObject* object, const Unreal::TCHAR* command, FOutputDevice& Ar, UObject* executor)
+    {
+        if (command[0] == '/' || command[0] == '!') {
+            command++; // Exclude the first character from the array
+
+            int same = ModConsole::CheckCommand(Ar, command);
+
+            if (same == 1)
+            {
+                Output::send<LogLevel::Verbose>(STR("Command detected with success : {} {}\n"), command, same);
+                Output::send<LogLevel::Verbose>(STR("connection accepted !\n"));
+            }
+            else
+            {
+                Output::send<LogLevel::Verbose>(STR("Command detected with difference : {} {}\n"), command, same);
+            }
+
+            return true;
+        }
+
+        Output::send<LogLevel::Verbose>(STR("Command undetected\n"));
+        // StartArchipelagoConnexion("localhost:38281", "Choo-Choo charles", "YaranCCC", "");
+        return false;
     }
 
     auto on_unreal_init() -> void override
@@ -88,36 +125,14 @@ public:
         auto Object = UObjectGlobals::StaticFindObject<UObject*>(nullptr, nullptr, STR("/Script/CoreUObject.Object"));
         Output::send<LogLevel::Verbose>(STR("Object Name: {}\n"), Object->GetFullName());
 
-        Hook::RegisterProcessConsoleExecCallback([&](UObject* object, const Unreal::TCHAR* command, FOutputDevice& Ar, UObject* executor) -> bool
-        {
-            if (command[0] == '/' || command[0] == '!') {
-                command++; // Exclude the first character from the array
-
-                int same = ModConsole::CheckCommand(Ar, command);
-
-                if(same == 1)
-                {
-                    Output::send<LogLevel::Verbose>(STR("Command detected with success : {} {}\n"), command, same);
-                    Output::send<LogLevel::Verbose>(STR("connection accepted !\n"));
-                }
-                else
-                {
-                    Output::send<LogLevel::Verbose>(STR("Command detected with difference : {} {}\n"), command, same);
-                }
-
-                return true;
-            }
-
-            Output::send<LogLevel::Verbose>(STR("Command undetected\n"));
-            // StartArchipelagoConnexion("localhost", "Choo-Choo charles", "YaranCCC", "");
-            return false;
-        });
+        Hook::RegisterProcessConsoleExecCallback(CallbackConsole);
 
         /// TEST OF HOOKS
-        if (Unreal::UObject::ProcessLocalScriptFunctionInternal.is_ready() && Unreal::Version::IsAtLeast(4, 22))
+        if (UObject::ProcessLocalScriptFunctionInternal.is_ready() && Unreal::Version::IsAtLeast(4, 22))
         {
             Output::send(STR("Enabling custom events\n"));
-            Unreal::Hook::RegisterProcessLocalScriptFunctionPostCallback(CallbackFunctionHook);
+            Hook::RegisterProcessLocalScriptFunctionPostCallback(CallbackFunctionHook);
+            // UObjectGlobals::RegisterHook("FuncNameToHook", NULL, NULL, NULL);
         }
     }
 
--- ModSrc/RandomizerMod/src/ModConsole.cpp
+++ ModSrc/RandomizerMod/src/ModConsole.cpp
@@ -1,42 +1,80 @@
 #include "ModConsole.hpp"
 
 
-static const TCHAR* CompareAndParseCmd(const TCHAR* command, const char* expectedCmd, const char separator)
-{
-    int i = 0;
+#define MAX_OPTION_LENGTH 50
+#define NB_MAX_OPTIONS 5
 
+
+/**
+*   @param command : The input command to compare and parse
+*   @param expectedCmd : The expected command name to compare with the input command
+*   @param outOptions : An out array copying the parsed options separated with '\0'
+*   @param outOptionPositions : An out array with the positions of all options in outOptions (-1 if no option at the position)
+*   @note If an option exceeds MAX_OPTION_LENGTH char or if more options than NB_MAX_OPTIONS are found, -1 is returned
+*   @note Get an option with outOptions[outOptionPositions[i]]) with i from 0 to the returned number of parsed options
+* 
+*   @todo This function should be optimized by comparing the input command wuth all known command names at once
+* 
+*   @return The number of parsed options, -1 if the command is not correct
+*/
+static int CompareAndParseCmd(const TCHAR* command, const char* expectedCmd, TCHAR outOptions[NB_MAX_OPTIONS * MAX_OPTION_LENGTH], int outOptionPositions[NB_MAX_OPTIONS])
+{
+    const char separator = ' ';
+    int i = 0, j = 0;
+    int numberOfParsedOptions = 0;
+
+    // Check if the first word of the command is known (correctly written)
     while (expectedCmd[i] != '\0')
     {
         // If a char is different, the command is not recognized
         if (command[i] != expectedCmd[i])
         {
-            return NULL;
+            return -1;
         }
         i++;
     }
 
-    if (command[i] == separator) // Command recognized, check if the separator is found
+    // While the end of the command is not reached, parse the options
+    while (command[i] != '\n' && command[i] != '\0')
     {
-        return command+i+1;
+        outOptionPositions[numberOfParsedOptions] = j;
+
+        // Ignore trailing separators after comparision, the command name is recognized
+        while (command[i] == separator)
+        {
+            i++;
+        }
+
+        // Copy the found option until the next separator or the end of the command 
+        while (command[i] != separator && command[i] != '\n' && command[i] != '\0')
+        {
+            outOptions[j] = command[i];
+            i++;
+            j++;
+        }
+
+        outOptions[j] = '\0';
+        j++;
+        numberOfParsedOptions++;
     }
-    else if (command[i] == '\n' || command[i] == '\0') // The end of command is reached
-    {
-        return command+i;
-    }
-    return NULL;
+
+    return numberOfParsedOptions;
 }
 
 
 namespace ModConsole {
     int ModConsole::CheckCommand(FOutputDevice& Ar, const TCHAR* command)
     {
-        const TCHAR* parsedCmd = CompareAndParseCmd(command, "help", ' ');
-        if (parsedCmd != NULL)
+        TCHAR outOptions[NB_MAX_OPTIONS * MAX_OPTION_LENGTH];
+        int outOptionPositions[NB_MAX_OPTIONS];
+
+        int numberOfOptions = CompareAndParseCmd(command, "help", outOptions, outOptionPositions);
+        if (numberOfOptions != -1)
         {
             // Print in console
             Ar.Log(STR("/help\n"));
             Ar.Log(STR("        Shows all available commands\n"));
-            Ar.Log(STR("/connect <IP>\n"));
+            Ar.Log(STR("/connect <IP> <PlayerName> [Password]\n"));
             Ar.Log(STR("        Connect to the room at the given <IP>\n"));
             Ar.Log(STR("/disconnect\n"));
             Ar.Log(STR("        Disconnect from the current room.\n"));
@@ -52,14 +90,22 @@ namespace ModConsole {
             Ar.Log(STR("        Get the given <item> and add it to inventory.\n"));
         }
         // Check if the use input is "/connect [...]"
-        // Example : /connect archipelago.gg:59157
-        parsedCmd = CompareAndParseCmd(command, "connect", ' ');
-        if (parsedCmd != NULL)
+        // Example : /connect archipelago.gg:59157 YaranCCC
+        numberOfOptions = CompareAndParseCmd(command, "connect", outOptions, outOptionPositions);
+        if (numberOfOptions >= 2) // At least 2 options are required for the "connect" command
         {
-            if (*parsedCmd == '\0') // Check the end of command is reached
-                Ar.Log(STR("OK\n"));
-            else // Otherwise, the command has an option
-                Ar.Log(STR("KO\n"));
+            Ar.Log(STR("Command recognized\n"));
+            TCHAR *optionIP = outOptions + outOptionPositions[0];
+            TCHAR *optionPlayerName = outOptions + outOptionPositions[1];
+            TCHAR *optionPassword = outOptions + outOptionPositions[2];
+            
+            // TODO : Remove this once the debug is finised
+            Ar.Log(STR("Option1 :\n"));
+            Ar.Log(optionIP);
+            Ar.Log(STR("Option2 :\n"));
+            Ar.Log(optionPlayerName);
+            Ar.Log(STR("Option3 :\n"));
+            Ar.Log(optionPassword);
             
             // To use after being connected
             // AP_SetItemClearCallback(void (*f_itemclr)());
@@ -69,31 +115,31 @@ namespace ModConsole {
 
             return 1;
         }
-        else if (CompareAndParseCmd(command, "disconnect", ' '))
+        else if (CompareAndParseCmd(command, "disconnect", outOptions, outOptionPositions))
         {
             /* code */
         }
-        else if (CompareAndParseCmd(command, "release", ' '))
+        else if (CompareAndParseCmd(command, "release", outOptions, outOptionPositions))
         {
             /* code */
         }
-        else if (CompareAndParseCmd(command, "collect", ' '))
+        else if (CompareAndParseCmd(command, "collect", outOptions, outOptionPositions))
         {
             /* code */
         }
-        else if (CompareAndParseCmd(command, "hint", ' '))
+        else if (CompareAndParseCmd(command, "hint", outOptions, outOptionPositions))
         {
             /* code */
         }
-        else if (CompareAndParseCmd(command, "hint_location", ' '))
+        else if (CompareAndParseCmd(command, "hint_location", outOptions, outOptionPositions))
         {
             /* code */
         }
-        else if (CompareAndParseCmd(command, "remaining", ' '))
+        else if (CompareAndParseCmd(command, "remaining", outOptions, outOptionPositions))
         {
             /* code */
         }
-		else if (CompareAndParseCmd(command, "get", ' ')) // get an item as done check
+		else if (CompareAndParseCmd(command, "get", outOptions, outOptionPositions)) // get an item as done check
         {
             /* code */
         }
